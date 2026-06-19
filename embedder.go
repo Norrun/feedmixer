5099b579bac07fd0dd99fb62eeaad4380179717f
Norrun <trulvens@tutanota.com>
2026-06-19 23:08:21 +0200

    updated embed FS to handle sql schema instead

--- embedder.go
+++ embedder.go
@@ -2,45 +2,17 @@ package feedmixer
 
 import (
 	"embed"
-	"errors"
 	"io/fs"
-	"os"
-
-	"github.com/Norrun/feedmixer/internal/must"
 )
 
 var prod bool
 var envSet bool
 
-//go:embed templates/*
-var buildFiles embed.FS
+//go:embed sql/schema/*.sql
+var sqldata embed.FS
 
-var fileSys fs.FS
+func GetFileSys() fs.FS {
 
-func GetFileSys() (_ fs.FS, erret error) {
-	defer func() {
-		erret = must.MustHandle(erret)
-	}()
+	return sqldata
 
-	if prod {
-		return buildFiles, nil
-	}
-	if fileSys != nil {
-		return fileSys, nil
-	}
-	wd, err := os.Getwd()
-	if err != nil {
-		return nil, err
-	}
-	fileSys = os.DirFS(wd)
-
-	return fileSys, nil
-}
-
-func SetEnv(production bool) error {
-	if envSet {
-		return errors.New("already configured")
-	}
-	prod = production
-	return nil
 }
