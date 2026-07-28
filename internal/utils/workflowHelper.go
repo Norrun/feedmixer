28287a48568e337df5cb5545f7a9d4012d8e5488
Norrun <trulvens@tutanota.com>
2026-07-29 00:18:44 +0200

    added another helper function

--- internal/utils/workflowHelper.go
+++ internal/utils/workflowHelper.go
@@ -2,13 +2,14 @@ package utils
 
 import (
 	"runtime"
+	"sync"
 )
 
 func FanOut[TIn, TOut any](done <-chan struct{}, source <-chan TIn, load, perTheread int, process func(in TIn) TOut) []<-chan TOut {
 
 	num := min(load, runtime.NumCPU()*perTheread)
 	res := make([]<-chan TOut, num)
-	for i := 0; i < num; i++ {
+	for i := range num {
 		channel := make(chan TOut)
 		go func() {
 			select {
@@ -21,4 +22,32 @@ func FanOut[TIn, TOut any](done <-chan struct{}, source <-chan TIn, load, perThe
 		res[i] = channel
 	}
 	return res
+}
+
+func Fan[TIn, TOut any](done <-chan struct{}, load, perTheread int, process func(in TIn) TOut, sources ...<-chan TIn) []<-chan TOut {
+	return FanOut(done, FanIn(done, sources...), max(len(sources), load), perTheread, process)
+}
+
+func FanIn[T any](done <-chan struct{}, sources ...<-chan T) <-chan T {
+	wg := sync.WaitGroup{}
+	fannedIn := make(chan T)
+	transfer := func(c <-chan T) {
+		defer wg.Done()
+		for v := range c {
+			select {
+			case <-done:
+				return
+			case fannedIn <- v:
+			}
+		}
+	}
+	for _, v := range sources {
+		wg.Add(1)
+		go transfer(v)
+	}
+	go func() {
+		wg.Wait()
+		close(fannedIn)
+	}()
+	return fannedIn
 }
