50859558f141f121966d9374069f7c93ff732604
Norrun <trulvens@tutanota.com>
2026-08-24 21:51:42 +0200

    added adding item to database

--- sql/queries/items.sql
+++ sql/queries/items.sql
@@ -1,2 +1,16 @@
 -- name: GetAllItems :many
 SELECT * FROM items;
+
+-- name: AddItem :one
+INSERT INTO items ( 
+    created_at, updated_at, title, url, description, published_at,feed_id
+    ) VALUES (
+    current_timestamp,
+    current_timestamp,
+    ?,
+    ?,
+    ?,
+    ?,
+    ?
+)
+RETURNING *;
