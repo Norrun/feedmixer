19b33b785159e5742a1fc0823fbece53f8b241db
Norrun <trulvens@tutanota.com>
2026-09-16 22:57:52 +0200

    remade WIP to fetching single threded

--- internal/feedprocessing/fetchfeeds.go
+++ internal/feedprocessing/fetchfeeds.go
@@ -1,7 +1,10 @@
 package feedprocessing
 
 import (
+	"context"
+	"database/sql"
 	"runtime"
+	"time"
 
 	"github.com/Norrun/feedmixer/internal/database"
 	"github.com/Norrun/feedmixer/internal/datautils"
@@ -9,8 +12,39 @@ import (
 	"github.com/mmcdole/gofeed"
 )
 
-func FetchFeedsAndSave()  {
-	
+func FetchFeedsAndSave(feeds []database.Feed, db *database.Queries) {
+
+	var errec []struct {
+		ref int64
+		err error
+	}
+	for _, dbf := range feeds {
+		parser := gofeed.NewParser()
+		feed, err := parser.ParseURL(dbf.Url)
+		if err != nil {
+			errec = append(errec, struct {
+				ref int64
+				err error
+			}{dbf.ID, err})
+		}
+		for _, v := range feed.Items {
+
+			//defer can()
+			hasPublishTime := v.PublishedParsed != nil
+			publishedNormalized := ""
+			if hasPublishTime {
+				publishedNormalized = v.PublishedParsed.Format(time.RFC3339)
+			}
+			db.AddItem(context.Background(), database.AddItemParams{
+				Title:       v.Title,
+				Url:         v.Link,
+				Description: sql.NullString{String: v.Description, Valid: v.Description != ""},
+				PublishedAt: sql.NullString{String: publishedNormalized, Valid: hasPublishTime},
+				FeedID:      dbf.ID,
+			})
+
+		}
+	}
 }
 
 func FetchFeeds(done <-chan struct{}, feeds []database.Feed) <-chan datautils.Result[*gofeed.Feed] {
@@ -21,37 +55,34 @@ func FetchFeeds(done <-chan struct{}, feeds []database.Feed) <-chan datautils.Re
 		feed, err := parser.ParseURL(s.Url)
 		return datautils.NewResult(feed, err)
 	})
-	return utils.FanIn[ datautils.Result[*gofeed.Feed]](done, chans...)
+	return utils.FanIn[datautils.Result[*gofeed.Feed]](done, chans...)
 
 }
-func SaveItemsFromFeeds()  {
-	
-	utils.FanOut(done, , threads, ) any {
-		if r.Err != nil {
-			return r.Err
-		}
-		// add feed data
-		for _, v := range r.Value.Items {
-			ctx, can := context.WithCancel(context.Background())
-			utils.ChainDoneContextCancel(done, can)
-			//defer can()
-			hasPublishTime := v.PublishedParsed != nil
-			publishedNormalized := ""
-			if hasPublishTime {
-				publishedNormalized = v.PublishedParsed.Format(time.RFC3339)
+func SaveItemsFromFeeds() {
+	/*
+		utils.FanOut(done, , threads, ) any {
+			if r.Err != nil {
+				return r.Err
 			}
-			db.AddItem(ctx, database.AddItemParams{
-				Title:       v.Title,
-				Url:         v.Link,
-				Description: sql.NullString{String: v.Description, Valid: v.Description != ""},
-				PublishedAt: sql.NullString{String: publishedNormalized, Valid: hasPublishTime},
-				FeedID:      r.Value.db.ID,
-			})
+			// add feed data
+			for _, v := range r.Value.Items {
+				ctx, can := context.WithCancel(context.Background())
+				utils.ChainDoneContextCancel(done, can)
+				//defer can()
+				hasPublishTime := v.PublishedParsed != nil
+				publishedNormalized := ""
+				if hasPublishTime {
+					publishedNormalized = v.PublishedParsed.Format(time.RFC3339)
+				}
+				db.AddItem(ctx, database.AddItemParams{
+					Title:       v.Title,
+					Url:         v.Link,
+					Description: sql.NullString{String: v.Description, Valid: v.Description != ""},
+					PublishedAt: sql.NullString{String: publishedNormalized, Valid: hasPublishTime},
+					FeedID:      r.Value.db.ID,
+				})
 
-		}
-		return nil
-	})
+			}
+			return nil
+		})*/
 }
-
-
-
